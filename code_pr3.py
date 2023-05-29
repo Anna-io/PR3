06426de77da3eebafbea8f02e81c6689b439ddd1
Anna-io <128488548+Anna-io@users.noreply.github.com>
2023-05-29 10:44:52 +0300

    Update code_pr3.py

--- code_pr3.py
+++ code_pr3.py
@@ -3,7 +3,7 @@ import requests
 api_key = "YOUR_API_KEY"
 city = "Kharkiv"
 
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
 response = requests.get(url)
 
 if response.status_code == 200:
